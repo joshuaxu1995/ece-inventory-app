a50d95fdfc2f88263048c5a22b6b7971c38e1f5b
joshuaxu1995 <joshxu95@gmail.com>
2017-02-02 20:56:49 -0500

    merged with master

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
-class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+class UserTest < ActiveSupport::TestCase
+
+
+  def setup
+    @item = Item.create!(
+      unique_name: "d",
+      quantity: 51,
+      model_number: 34221,
+      description: "broken glass",
+      tags: {
+          tagarray: ["tag1", "tag2", "tag3"]
+      },
+      instances: {
+          instancearray: ["instance1", "instance2", "instance3"]
+      }
+    )
+  end
+  # test "item should be unique" do
+  #   duplicate_item = @item.dup
+  #   #Testing for case insensitivity
+  #   duplicate_item.unique_name = @item.unique_name.upcase
+  #   @item.save
+  #   assert_not duplicate_item.valid?
   # end
+
+  # test "item should not be that long" do
+  #   @item.unique_name = "q" * 51
+  #   assert_not @item.valid?
+  # end
+
+  test "item quantity should not be negative" do
+    @item.quantity = -51
+    assert_not @item.valid?
+  end
+
+
 end
